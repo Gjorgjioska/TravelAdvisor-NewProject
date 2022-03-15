149bf3dc2a3e0e9214ad17aec653582e0422e0f4
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-15 19:24:15 +0100

    Fetching data api

---
+++ src/api/index.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+const options = {
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+    "x-rapidapi-key": "d1638c5894msh583c9f818d7b7a5p1b7152jsna9de0a72a63a",
+  },
+};
+
+export const getPlacesData = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, options);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
